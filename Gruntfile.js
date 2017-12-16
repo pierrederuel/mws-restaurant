f7c5c9f955af584cd95b2ea9fec967e637658981
Kostas Kapetanakis <kapekost@gmail.com>
2017-12-16 02:45:33 +0000

    chore: cleanup grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,12 +3,10 @@ module.exports = function (grunt) {
     const server_port = 8000;
     // Project configuration.
     grunt.initConfig({
+        // Prepare dist folder
         clean: {
             dist: {
                 folder: ['dist/'],
-            },
-            images: {
-                src: ['images'],
             }
         },
         connect: {
@@ -39,7 +37,9 @@ module.exports = function (grunt) {
                         }
                     ]
                 },
-                //replace the google maps key from the environment to the 2 html files that are using it
+                // Replace the google maps key
+                // from the environment variables
+                // replace the server port number from this config
                 files: [{
                     expand: true,
                     cwd: './dist',
@@ -48,6 +48,7 @@ module.exports = function (grunt) {
                 }]
             }
         },
+        // Copy static files except images
         copy: {
             static: {
                 files: [{
@@ -59,7 +60,7 @@ module.exports = function (grunt) {
 
             }
         },
-
+        // Process images
         responsive_images: {
             large_high: {
                 options: {
@@ -127,7 +128,7 @@ module.exports = function (grunt) {
             }
         },
 
-        /* Generate the images directory if it is missing */
+        // Generate the images directory if it is missing
         mkdir: {
             images: {
                 options: {
@@ -146,16 +147,14 @@ module.exports = function (grunt) {
     grunt.loadNpmTasks('grunt-mkdir');
     grunt.loadNpmTasks('grunt-responsive-images');
 
-    // Start web server
-    grunt.registerTask('serve', ['build', 'connect', 'watch']);
-    grunt.registerTask('build', ['clean:dist', 'copy:static', 'replace']);
     grunt.registerTask('optimize_images', [
-        'clean:images',
         'mkdir:images',
         'responsive_images:large_high',
         'responsive_images:large_low',
         'responsive_images:small_high',
         'responsive_images:small_low'
-    ])
+    ]);
+    grunt.registerTask('serve', ['build', 'connect', 'watch']);
+    grunt.registerTask('build', ['clean:dist', 'copy:static', 'replace', 'optimize_images']);
 
 };
