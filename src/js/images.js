4275f201c143423ab33e8e2e22fc86fd7b87aebf
Kostas Kapetanakis <kapekost@gmail.com>
2018-01-07 02:19:50 +0000

    feat: update images.js

--- src/js/images.js
+++ src/js/images.js
@@ -20,31 +20,31 @@ class ImageInfo {
       },
       3: {
         "id": 3,
-        "photograph": "3.jpg",
+        "photograph": "3",
         "alt": "the main restaurant area empty",
         "caption": "Modern design to enjoy your meals",
       },
       4: {
         "id": 4,
-        "photograph": "4.jpg",
+        "photograph": "4",
         "alt": "the entrance of the shop",
         "caption": "The corner shop, easy to spot"
       },
       5: {
         "id": 5,
-        "photograph": "5.jpg",
+        "photograph": "5",
         "alt": "inside the shop, customers and staff",
         "caption": "Meet our friendly staff"
       },
       6: {
         "id": 6,
-        "photograph": "6.jpg",
+        "photograph": "6",
         "alt": "the area inside with some people queueing and other eating",
         "caption": "Large space for big groups"
       },
       7: {
         "id": 7,
-        "photograph": "7.jpg",
+        "photograph": "7",
         "alt": "black and white, the display of the shop with 2 people passing by",
         "caption": "Our classic shop, still keeping our values"
       },
@@ -52,17 +52,17 @@ class ImageInfo {
         "id": 8,
         "name": "The Dutch",
         "neighborhood": "Manhattan",
-        "photograph": "8.jpg"
+        "photograph": "8"
       },
       9: {
         "id": 9,
-        "photograph": "9.jpg",
+        "photograph": "9",
         "caption": "Enjoying our home made meals",
         "alt": "black and white, people eating in the restaurant"
       },
       10: {
         "id": 10,
-        "photograph": "10.jpg",
+        "photograph": "10",
         "alt": "the area inside the shop, the furniture and bar, without people",
         "caption": "Modern space, ready to serve your appetite"
       }
